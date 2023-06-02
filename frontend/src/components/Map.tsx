a4a754a2801e0ff0257ed153443e4ed8751756a8
Olusoji <esohjay3@gmail.com>
2023-06-02 16:10:59 +0100

    fix marker display

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -16,34 +16,21 @@ const Map: React.FC<MapProps> = ({ center, zoom, geojsonData }) => {
     mapRef.current = L.map("map", {
       center,
       zoom,
+      zoomSnap: 0,
+      dragging: true,
     });
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution: "Map data &copy; OpenStreetMap contributors",
     }).addTo(mapRef.current);
+    if (mapRef.current && geojsonData.type === "Feature") {
+      const latLng = L.latLng(
+        geojsonData.geometry.coordinates[1],
+        geojsonData.geometry.coordinates[0]
+      );
+      console.log(latLng);
+      // L.marker(latLng).addTo(mapRef?.current as L.Map);
 
-    return () => {
-      if (mapRef.current) {
-        mapRef.current.remove();
-      }
-    };
-  }, [center, zoom]);
-
-  const calculateDistance = (
-    latLng1: L.LatLngExpression,
-    latLng2: L.LatLngExpression
-  ): number => {
-    return mapRef.current?.distance(latLng1, latLng2) || 0;
-  };
-
-  if (mapRef.current && geojsonData.type === "Feature") {
-    const latLng = L.latLng(
-      geojsonData.geometry.coordinates[1],
-      geojsonData.geometry.coordinates[0]
-    );
-    console.log(latLng);
-    // L.marker(latLng).addTo(mapRef?.current as L.Map);
-    setTimeout(() => {
       const customIcon = L.icon({
         iconUrl: icon, // Replace with the correct path to your marker icon image
         iconSize: [25, 41],
@@ -52,8 +39,21 @@ const Map: React.FC<MapProps> = ({ center, zoom, geojsonData }) => {
       });
 
       L.marker(latLng, { icon: customIcon }).addTo(mapRef.current!);
-    }, 0);
-  }
+    }
+
+    return () => {
+      if (mapRef.current) {
+        mapRef.current.remove();
+      }
+    };
+  }, [center, zoom, geojsonData]);
+
+  const calculateDistance = (
+    latLng1: L.LatLngExpression,
+    latLng2: L.LatLngExpression
+  ): number => {
+    return mapRef.current?.distance(latLng1, latLng2) || 0;
+  };
 
   const addMarker = (latLng: L.LatLngExpression): void => {
     L.marker(latLng).addTo(mapRef.current as L.Map);
